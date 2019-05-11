944e0f6fb4441180fc78c404cf1c237bc8a6afe9
sulab thapa <tsgeek92@gmail.com>
2019-05-11 09:25:49 +0530

    Update SpeechBot.py

--- SpeechBot.py
+++ SpeechBot.py
@@ -41,6 +41,7 @@ def takeCommand():
     with sr.Microphone() as source:
         print("Listening.......")
         r.pause_threshold= 1
+        r.adjust_for_ambient_noise(source,duration=1)
         audio = r.listen(source)
     
     try:
